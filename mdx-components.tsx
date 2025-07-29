82379220892d1ef117c691b7454c273927ab5968
llayz46 <layzer@outlook.fr>
2025-07-29 15:29:52 +0200

    enhance documentation by adding detailed installation and seeder guides for Swell, improving user onboarding and data initialization processes

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,12 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import * as icons from 'lucide-react';
 import * as FilesComponents from 'fumadocs-ui/components/files';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
+    ...(icons as unknown as MDXComponents),
     ...defaultMdxComponents,
     ...FilesComponents,
     ...components,
