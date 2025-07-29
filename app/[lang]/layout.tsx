235d07daf978776077ba94bfeee3767798b0747d
llayz46 <layzer@outlook.fr>
2025-07-29 20:06:30 +0200

    add @vercel/analytics package and integrate analytics component into layout for improved performance tracking

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import '@app/global.css';
 import {RootProvider} from 'fumadocs-ui/provider';
 import {Geist, Geist_Mono} from 'next/font/google';
@@ -48,6 +49,7 @@ export default async function RootLayout({ params, children }: { params: Promise
                 >
                     {children}
                     <SpeedInsights />
+                    <Analytics />
                 </RootProvider>
             </body>
         </html>
